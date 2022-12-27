7bec6831d4802b48c41305176a5e1ccb0f3beb80
jansen her <jansen.cs@gmail.com>
2022-12-27 08:26:55 +0800

    Use inspect analyzer.

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -5,28 +5,31 @@ import (
 	"strings"
 
 	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
 )
 
 var Analyzer = &analysis.Analyzer{
-	Name: "Simple",
-	Doc:  "Tests that no function name contains silly",
-	Run:  run,
+	Name:     "Simple",
+	Doc:      "Tests that no function name contains silly",
+	Run:      run,
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
-	for _, f := range pass.Files {
-		ast.Inspect(f, func(n ast.Node) bool {
-			f, ok := n.(*ast.FuncDecl)
-			if !ok {
-				return true
-			}
-			if strings.Contains(strings.ToLower(f.Name.Name), "silly") {
-				pass.Reportf(n.Pos(), "function '%s', should not contain silly", f.Name.Name)
-			}
+	// The ResultOf field provides the results computed by the analyzers required by this one,
+	// as expressed in its Analyzer.Requires field.
 
-			return true
-		})
-	}
+	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	inspector.Preorder([]ast.Node{(*ast.FuncDecl)(nil)}, func(n ast.Node) {
+		f, ok := n.(*ast.FuncDecl)
+		if !ok {
+			return
+		}
+		if strings.Contains(strings.ToLower(f.Name.Name), "silly") {
+			pass.Reportf(n.Pos(), "function '%s', should not contain silly", f.Name.Name)
+		}
+	})
 
 	return nil, nil
 }
